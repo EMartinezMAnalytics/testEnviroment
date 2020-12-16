6768dd829f42462f82cda881bd18080fed601321
unknown <emartinez@masanalytics.com>
2020-12-16 12:32:49 -0500

    pruebas

--- envtesting/__init__.py
+++ envtesting/__init__.py
@@ -7,6 +7,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     nombre_env= os.environ['NOMBRE']
     return func.HttpResponse(
-        f"El ambiente se llama: {nombre_env}",
+        f"El ambiente se llama---> {nombre_env}",
         status_code=200
     )
